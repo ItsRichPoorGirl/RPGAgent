eb14e83e3ae309eb26b2e16d06e2227b40f49d14
ItsRichPoorGirl <157065618+ItsRichPoorGirl@users.noreply.github.com>
2025-05-23 23:07:32 -0700

    update docker image

--- backend/sandbox/sandbox.py
+++ backend/sandbox/sandbox.py
@@ -91,7 +91,7 @@ def create_sandbox(password: str, project_id: str = None):
         labels = {'id': project_id}
 
     params = CreateSandboxParams(
-        image="luciq/luciq:0.1.2",
+        image="ghcr.io/sunaai/suna:latest",
         public=True,
         labels=labels,
         env_vars={
